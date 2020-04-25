4f2c7a12af6f146ecece03259e49db2053f39d0e
Daniel Glus <danielhglus@gmail.com>
2020-04-25 04:55:38 -0400

    fix for apostrophes in user names

--- reply-link.js
+++ reply-link.js
@@ -647,7 +647,7 @@ function loadReplyLink( $, mw ) {
                 newHref = "./" + REDLINK_HREF_RGX.exec( liveHref )[1];
             }
         }
-        newHref = newHref.replace( /'/g, "\\'" ).replace( /\\/g, "\\\\" );
+        newHref = newHref.replace( /\\/g, "\\\\" ).replace( /'/g, "\\'" );
         var livePath = ascendToCommentContainer( sigLinkElem, /* live */ true, /* recordPath */ true );
         corrCmtDebug.newHref = newHref; corrCmtDebug.livePath = livePath;
 
